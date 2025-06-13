d3fc72ea9588b9ff554a4578d1dd5c6a31a85e50
CR0B0T <caio4arp10@gmail.com>
2025-06-13 12:25:21 -0300

    Defining actor type

--- module/runarcana.js
+++ module/runarcana.js
@@ -1,8 +1,19 @@
+class RunarcanaActorSheet extends ActorSheet {
+  get template() {
+    return "templates/sheets/actor-sheet.html";
+  }
+
+  getData() {
+    const data = super.getData();
+    return data;
+  }
+}
+
 Hooks.once("init", () => {
   console.log("Runarcana | Inicializando o sistema Runarcana...");
 
-  CONFIG.Actor.documentClass = RunarcanaActor;
+  CONFIG.Actor.documentClass = Actor;
 
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("runarcana", RunarcanaActorSheet, { makeDefault: true });
+  Actors.registerSheet("runarcana", RunarcanaActorSheet, { types: ["character"], makeDefault: true });
 });
